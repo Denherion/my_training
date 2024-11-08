14fb4ee8c70119264348ebb2c89497b4adf27c71
Denherion <ydenisk@gmail.com>
2024-11-08 12:47:26 +1000

    Update basic.py

--- basic.py
+++ basic.py
@@ -7,6 +7,4 @@ print(2 * 2 + 2)
 print(2 * (2 + 2))
 print((2 * 2 + 2) == (2 * (2 + 2)))
 # 4th program
-print(float('123.456'))
-print(123.456 * 10)
-print(int(1234.56 % 10))
+print(int(float('123.456') * 10) % 10)
